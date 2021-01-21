36bd552bb1da5fec663da839685b6c51d1832a78
shingoishimura <shingoishimuratw@gmail.com>
2021-01-22 04:37:19 +0900

    ユーザー管理機能実装　新規登録　ログイン　ログアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,25 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  
-  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :items
+  has_many :pay_infos
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers' 
+  
+  validates :nickname,presence: true
+  
+  with_options presence: true,format: { with:/^[ぁ-んァ-ヶー一-龠]+$/, multiline: true,message: 'Full-width characters' } do
+  validates :last_name
+  validates :first_name
+  end
+  with_options presence: true,format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,message: "Full-width katakana characters"}do
+  validates :last_name_kana
+  validates :first_name_kana
+  end
+  validates :birth_date,presence: true
+  
+                
 end
