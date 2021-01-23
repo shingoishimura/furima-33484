0ef618e4598d63f536766f2bf73beabc1785be02
shingoishimura <shingoishimuratw@gmail.com>
2021-01-24 01:15:18 +0900

    Rubocopで修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,20 +6,18 @@ class User < ApplicationRecord
   has_many :items
   has_many :pay_infos
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers' 
-  
-  validates :nickname,presence: true
-  
-  with_options presence: true,format: { with:/^[ぁ-んァ-ヶー一-龠]+$/, multiline: true,message: 'Full-width characters' } do
-  validates :last_name
-  validates :first_name
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+
+  validates :nickname, presence: true
+
+  with_options presence: true, format: { with: /^[ぁ-んァ-ヶー一-龠]+$/, multiline: true, message: 'Full-width characters' } do
+    validates :last_name
+    validates :first_name
   end
-  with_options presence: true,format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,message: "Full-width katakana characters"}do
-  validates :last_name_kana
-  validates :first_name_kana
+  with_options presence: true, format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/, message: 'Full-width katakana characters' } do
+    validates :last_name_kana
+    validates :first_name_kana
   end
-  validates :birth_date,presence: true
-  
-                
+  validates :birth_date, presence: true
 end
