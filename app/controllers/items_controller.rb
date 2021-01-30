1296ca6ba486030308031090c5345aaccc3b4f9e
shingoishimura <shingoishimuratw@gmail.com>
2021-01-30 09:36:36 +0900

    rubocopの修正を追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,30 +1,23 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit,:destroy]
-  before_action :set_item, only: [ :show, :edit, :update, :destroy]
-  
-
+  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
-    @items = Item.all.order("created_at DESC")
+    @items = Item.all.order('created_at DESC')
   end
 
   def show
-    
   end
-  
+
   def edit
-    
-   unless @item.user_id == current_user.id
-    redirect_to action: :index
-   end    
-  
+    redirect_to action: :index unless @item.user_id == current_user.id
   end
 
   def update
     if @item.update(item_params)
-    redirect_to item_path
-   
-  else
+      redirect_to item_path
+
+    else
       render :edit
     end
   end
@@ -46,20 +39,16 @@ class ItemsController < ApplicationController
   def destroy
     @item.destroy
     redirect_to root_path
-
-
   end
-
 
   private
 
   def item_params
-    params.require(:item).permit(:item_name, :text, :image, :category_id, :state_id, :send_fee_id, :prefecture_id,:send_period_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :text, :image, :category_id, :state_id, :send_fee_id, :prefecture_id,
+                                 :send_period_id, :price).merge(user_id: current_user.id)
   end
 
   def set_item
     @item = Item.find(params[:id])
   end
-
- 
 end
