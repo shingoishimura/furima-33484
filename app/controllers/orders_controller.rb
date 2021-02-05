20d8456ee87a0c7684685ad64f05c20f49c878bd
shingoishimura <shingoishimuratw@gmail.com>
2021-02-05 16:41:08 +0900

    商品購入機能実装　修正４

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,20 +1,14 @@
 class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
+  before_action :authenticate_user!, only: [:index]
 
   def index
-    
-    if @item.order.present?
-      redirect_to root_path
-    elsif user_signed_in? && current_user.id == @item.user_id
-      redirect_to root_path
-    elsif user_signed_in?
-      @order_address = OrderAddress.new
-    else
-      redirect_to new_user_session_path
-    end
 
-    
+    if @item.order.present? || current_user.id == @item.user_id
+      redirect_to root_path
     end
+    @order_address = OrderAddress.new
+    
     
   end
 
